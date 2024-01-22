7c40dc989fa5716d6907ab96cc6797fa4e6527c0
20100 <96266750+Vincent-Durret@users.noreply.github.com>
2024-01-22 16:26:44 +0100

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const svgCaptcha = require("svg-captcha");
-
-const createCaptcha = () => {
-  return svgCaptcha.create();
-};
-
-const captcha = createCaptcha();
-console.log(captcha.data); // SVG data
-console.log("Captcha Text:", captcha.text);
