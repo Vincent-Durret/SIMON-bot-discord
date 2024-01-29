fde378a70b350cff23b8a8344d3d706e39b5a610
GareFunkle <vincent.durret@gmail.com>
2024-01-29 10:35:30 +0100

    //

--- Commandes/ping.js
+++ Commandes/ping.js
@@ -4,7 +4,7 @@ module.exports = {
   data: new Discord.SlashCommandBuilder()
   .setName("ping")
   .setDescription("Affiche la latence")
-  setDefaultMemberPermissions("aucune")
+
 
   async execute(bot, interaction, args) {
     await interaction.reply(`Ping : \`${bot.ws.ping}\``);
