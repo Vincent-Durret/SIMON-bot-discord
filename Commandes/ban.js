a5fab45864f5976852a2d9bc74f038b48c1002de
GareFunkle <vincent.durret@gmail.com>
2024-01-29 09:23:07 +0100

    change ban

--- Commandes/ban.js
+++ Commandes/ban.js
@@ -84,18 +84,38 @@ module.exports = {
     }
 
     // Envoyer un message à l'utilisateur banni, si possible
-    await interaction.editReply({
-      content: `${member.user.tag} a été banni pour la raison : \`${reason}\``,
-    });
+    try {
+      await member.send(
+        `Tu as été banni du serveur ${interaction.guild.name} pour la raison : \`${reason}\``
+      );
+      console.log(
+        `Tu as été banni du serveur ${interaction.guild.name} pour la raison : \`${reason}\``
+      );
+    } catch (err) {
+      console.error("Erreur lors de l'envoi du message de bannissement :", err);
+    }
 
     // Effectuer le bannissement
     await member.ban({ reason: reason });
 
+    await interaction.editReply({
+      content: `${member.user.tag} a été banni pour la raison : \`${reason}\``,
+    });
+
     // Confirmer le bannissement
     try {
-      await member.send(`Message de bannissement`);
+      await interaction.reply({
+        content: `${member.user.tag} a été banni pour la raison : \`${reason}\``,
+        ephemeral: true,
+      });
+      console.log(
+        `${member.user.tag} a été banni pour la raison : \`${reason}\``
+      );
     } catch (err) {
-      console.error("Erreur lors de l'envoi du message de bannissement :", err);
+      console.log(
+        `${member.user.tag} n'a  pas pu être banni pour la raison :`,
+        err
+      );
     }
   },
 };
